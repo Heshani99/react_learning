c29bdcd352d76154689b352994c39e7a0dfd6ca5
Heshani99 <heshaniwick99@gmail.com>
2022-03-07 15:48:59 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import NavBar from './Components/NavBar';
+import Body from './Components/Body';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Body/>
       <h1>Uththara thama hondatama kale</h1>
     </div>
   );
---
+++ src/Components/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Body(){
+    return(
+        <React.Fragment>
+        <div>Body</div>
+        <div>Hello</div>
+        </React.Fragment>
+        
+        
+
+    )
+}
+
+export default Body
